0e0c0ef62d384b085be98030e144b79cba4ba88b
errplane <110797323+errplanedev@users.noreply.github.com>
2024-04-21 09:21:54 -0700

    darker

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import './App.css'
 function App() {
   return (
     <div>
-      <h1 className="text-3xl"><span className="text-green-400">Nobody</span> deserves to end their own life.</h1>
+      <h1 className="text-3xl"><span className="text-green-500">Nobody</span> deserves to end their own life.</h1>
       <p>
         If someone tells you to "KYS" (or something similar),
-        <span className="text-green-400"> don't listen</span>.
+        <span className="text-green-500"> don't listen</span>.
       </p>
       <br />
       <p>If you are <i>ever</i> feeling suicidal or depressed, please dial <a href="https://blog.opencounseling.com/suicide-hotlines/">the appropriate suicide hotline for your area</a>.</p>
       <br />
       <hr />
       <br />
-      <p>Made with <span className="text-green-400">all</span> of <a href="https://dour.com">errplane</a>'s ❤️</p>
+      <p>Made with <span className="text-green-500">all</span> of <a href="https://dour.com">errplane</a>'s ❤️</p>
     </div>
   )
 }
