ed04a2c75172ed6458fdfb88f86624c3daacb3ab
errplane <110797323+errplanedev@users.noreply.github.com>
2024-04-20 21:14:06 -0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl"><span className="text-green-400">Nobody</span> deserves to end their own life.</h1>
+      <p>
+        If someone tells you to "KYS" (or something similar),
+        <span className="text-green-400"> don't listen</span>.
+      </p>
+      <br />
+      <p>If you are <i>ever</i> feeling suicidal or depressed, please dial the appropriate suicide hotline for your area.</p>
+      <br />
+      <hr />
+      <br />
+      <p>Made with <span className="text-green-400">all</span> of <a href="/">errplane</a>'s ❤️</p>
+    </div>
+  )
+}
+
+export default App
